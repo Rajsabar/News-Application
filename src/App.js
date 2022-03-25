4ba1c2e39f0fa7d42f95b6a1039228672feeffb5
Rajsabar <87649623+Rajsabar@users.noreply.github.com>
2022-03-25 22:21:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import News from './Components/News';
+import Navbar from './Components/News';
+import React ,{Component} from 'react';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div> 
+      <Navbar/>
+      
+      
+      </div>
+    )
+  }
+}
